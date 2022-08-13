0d8f8ab8c953bdda007453a030b44021f81ae56e
Christian Varas <christianvaras.2020@gmail.com>
2022-08-12 23:54:22 -0500

    create a manifest that kills a process named killmenow

---
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -0,0 +1,7 @@
+# Create a manifest that kills a process named killmenow
+
+exec { 'killmenow':
+  command  => 'pkill killmenow',
+  path     => '/usr/local/bin:/usr/sbin:/bin'.
+  provider => 'shell',
+}
